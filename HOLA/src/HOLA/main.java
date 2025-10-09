d59e639ad9db498fb91b5f3cf5b34db2afd5daa7
Anas <anasmejahad15@gmail.com>
2025-10-09 16:53:50 +0200

    Guardamos la funcionalidad 1

--- HOLA/src/HOLA/main.java
+++ HOLA/src/HOLA/main.java
@@ -3,5 +3,6 @@ package HOLA;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hola, mundo!");
+        System.out.println("Funcionalidad 1 - paso 1");
     }
 }
