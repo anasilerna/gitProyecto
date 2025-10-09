8a912577421d1cb957861b01daf55224093ae96c
Anas <anasmejahad15@gmail.com>
2025-10-09 16:58:45 +0200

    "Funcionalidad 2 - paso 1"

--- HOLA/src/HOLA/main.java
+++ HOLA/src/HOLA/main.java
@@ -1,7 +1,8 @@
 package HOLA;
 
 public class main {
-    public static void main(String[] args) {
-        System.out.println("Hola, mundo!");
-    }
-}
+	  public static void main(String[] args) {
+	        System.out.println("Hola, mundo!");
+	        System.out.println("Funcionalidad 2 - paso 1");
+	    }
+	}
